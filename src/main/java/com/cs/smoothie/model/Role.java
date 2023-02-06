c4880595bcb27831de2078968814bce94622e173
andrzej.salamon@gmail.com <andrzej.salamon@gmail.com>
2023-02-06 09:27:42 +0100

    Role.java entity , RoleType.java enum

--- src/main/java/com/cs/smoothie/model/Role.java
+++ src/main/java/com/cs/smoothie/model/Role.java
@@ -1,4 +1,36 @@
 package com.cs.smoothie.model;
 
+
+import com.cs.smoothie.model.enums.RoleType;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "role")
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private RoleType name;
+
+    public Role() {
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public RoleType getName() {
+        return name;
+    }
+
+    public void setName(RoleType name) {
+        this.name = name;
+    }
 }
